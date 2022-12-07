d05519db56c1de03d9529c0e4c92215d5bd66545
wq-max <53320735+wq-max@users.noreply.github.com>
2022-12-07 21:25:59 +0800

    chapter5 end!

---
+++ new_start/chapter5/src/p4/Pipe.java
@@ -0,0 +1,63 @@
+package p4;
+
+import java.io.IOException;
+import java.io.PipedReader;
+import java.io.PipedWriter;
+
+public class Pipe {
+    static class ReaderThread implements Runnable {
+        private PipedReader reader;
+
+        public ReaderThread(PipedReader reader) {
+            this.reader = reader;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("this is reader");
+            int receive = 0;
+            try {
+                while ((receive = reader.read()) != -1) {
+                    System.out.println((char) receive);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    static class WriterThread implements Runnable {
+        private PipedWriter writer;
+
+        public WriterThread(PipedWriter writer) {
+            this.writer = writer;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("this is writer");
+            int receive = 0;
+            try {
+                writer.write("test");
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    writer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        PipedWriter writer = new PipedWriter();
+        PipedReader reader = new PipedReader();
+        writer.connect(reader);
+
+        new Thread(new ReaderThread(reader)).start();
+        Thread.sleep(1000);
+        new Thread(new WriterThread(writer)).start();
+    }
+}
